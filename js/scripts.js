8811c92eca88936e9a76323d1e7c3b7c68a05474
Davi <davifigueiredo0195@gmail.com>
2023-05-21 11:53:43 -0300

    dados de cadastro

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,52 @@
-/*!
-* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+let dados = [];
+console.log(dados);
+const salvarDados = () => {
+    let nome = document.getElementById("nome");
+    let data_nascimento = document.getElementById("data_nascimento");
+    let rg = document.getElementById("rg");
+    let cpf = document.getElementById("cpf");
+    let cnh = document.getElementById("cnh");
+    let antecedentes_criminais = document.getElementById("antecedentes_criminais");
+    let marca = document.getElementById("marca");
+    let modelo = document.getElementById("modelo");
+    let cor = document.getElementById("cor");
+    let placa = document.getElementById("placa");
+    let rua = document.getElementById("rua");
+    let numero = document.getElementById("numero");
+    let bairro = document.getElementById("bairro");
+    let cidade = document.getElementById("cidade");
+    let cep = document.getElementById("cep");
+    let nome2 = document.getElementById("nome2");
+    let data_nascimento2 = document.getElementById("data_nascimento2");
+    let idade = document.getElementById("idade");
+    let escola = document.getElementById("escola");
+    let secretaria = document.getElementById("secretaria");
+    let endereco = document.getElementById("endereco");
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+    dados.push({
+        'nome': nome.value,
+        'data_nascimento': data_nascimento.value,
+        'rg': rg.value,
+        'cpf': cpf.value,
+        'cnh': cnh.value,
+        'antecedentes_criminais': antecedentes_criminais.value,
+        'marca': marca.value,
+        'modelo': modelo.value,
+        'cor': cor.value,
+        'placa': placa.value,
+        'rua': rua.value,
+        'numero': numero.value,
+        'bairro': bairro.value,
+        'cidade': cidade.value,
+        'cep': cep.value,
+        'nome2': nome2.value,
+        'data_nascimento2': data_nascimento2.value,
+        'idade': idade.value,
+        'escola': escola.value,
+        'secretaria': secretaria.value,
+        'endereco': endereco.value
     });
 
-});
+
+    
+}
